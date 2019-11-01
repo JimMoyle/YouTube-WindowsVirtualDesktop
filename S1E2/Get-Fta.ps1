06f5fa9589bdee1de598869f53c55182b33701b1
JimMoyle <github@jimmoyle.com>
2019-11-01 13:42:55 +0000

    Accepting merges

--- S1E2/Get-Fta.ps1
+++ S1E2/Get-Fta.ps1
@@ -31,23 +31,13 @@ function Add-FslFtaInfo {
 
 }
 
-<<<<<<< HEAD
 Import-Module 'D:\PoSHCode\GitHub\FSLogix.Powershell.Rules\FSLogix.PowerShell.Rules\FSLogix.PowerShell.Rules.psd1' -Force
 
 $searchText = 'txtfile'
-=======
-Import-Module 'C:\PoShCode\FSLogix.Powershell.Rules\FSLogix.PowerShell.Rules\FSLogix.PowerShell.Rules.psd1'
-
-$searchText = 'VisioViewer.Viewer'
->>>>>>> d52123a835d5510899c866e8c3c1532b87237ce6
 $outPath = 'C:\JimM\FTA.fxr'
 
 $items = Get-ChildItem -Path HKLM:\SOFTWARE\Classes\ | Where-Object PSChildName -Like '.*'
 
 foreach ($item in $items) {
-<<<<<<< HEAD
     $extension = Get-ItemProperty -path $item.pspath | where-Object '(Default)' -Match $searchText | Select-Object -ExpandProperty PSChildname | Add-FslFtaInfo -outPath $outPath
-=======
-    Get-ItemProperty -path $item.pspath | where-Object '(Default)' -eq $searchText | Select-Object -ExpandProperty PSChildname | Add-FslFtaInfo -outPath $outPath
->>>>>>> d52123a835d5510899c866e8c3c1532b87237ce6
 }
